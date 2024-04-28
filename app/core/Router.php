c7d253323c56cee31085b18744f4d5d0d1b80d08
Arseny Mashavets <paceenirivu345@gmail.com>
2024-04-28 22:11:06 +0300

    Add method for routing

--- app/core/Router.php
+++ app/core/Router.php
@@ -3,8 +3,53 @@ namespace app\core;
 
 class Router
 {
+    private $routes = [];
+
+    private $params = [];
     public function __construct()
     {
+        $routes_arr = require_once 'app/config/routes.php';
+        foreach ($routes_arr as $route => $params) {
+            $this->add_pattern_route($route, $params);
+        }
+    }
 
+    private function add_pattern_route($route, $params)
+    {
+        $template_route = '#^' . trim($route, '/') . '$#';
+        $this->routes[$template_route] = $params;
+    }
+
+    private function match()
+    {
+        $url_width_query = trim($_SERVER['REQUEST_URI'], '/');
+        $url = $this->removeQueryString($url_width_query);
+        debug($url);
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function removeQueryString($url)
+    {
+        $parts = explode('?', $url);
+        return trim($parts[0], '/');
+    }
+
+    public function run()
+    {
+        if ($this->match()) {
+            $controller_name = "\app\controllers\\" . $this->params['controller'] . 'Controller';
+            // $controller_name = "\app\controllers\MainController";
+            if (class_exists($controller_name)) {
+                echo 'yes';
+            } else {
+                echo 'no';
+            }
+        }
     }
 }
--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
+require_once 'app/config/paths.php';
+include "app/lib/debug.php";
 use app\core\Router;
+
+
 
 spl_autoload_register(function ($class) {
     $class = str_replace('\\', '/', $class);
     require_once "{$class}.php";
 });
 
-new Router();
+$router = new Router();
+$router->run();
