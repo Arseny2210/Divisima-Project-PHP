b21148ad7d1e707a94a8f18bd126f0529aed79ba
Arseny Mashavets <paceenirivu345@gmail.com>
2024-06-03 10:58:33 +0300

    Ready TIK-4

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -7,8 +7,24 @@ class MainController extends Controller
 {
     public function indexAction()
     {
-        $banners = $this->model->get_banners();
-        $this->view->render($banners);
+        $banners_urls = $this->model->get_banners();
+        $features_urls = $this->model->get_features();
 
+        include LIB . '/texts/main.php';
+
+        $banners = $this->add_object_texts($banners_urls, $banners_texts);
+        $features = $this->add_object_texts($features_urls, $features_text);
+
+        $data = compact('banners', 'features');
+        $this->view->render((object) $data);
+
+    }
+
+    private function add_object_texts($data, $data_texts)
+    {
+        foreach ($data as $ind => $item) {
+            $item->texts = (object) $data_texts[$ind];
+        }
+        return $data;
     }
 }
